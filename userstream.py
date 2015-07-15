6835abb9c9631f33f7a3d6d09fb3886f0c2bdb48
oliver2213 <oliver2213@me.com>
2015-07-15 06:16:04 -0400

    Added userstream.py, needs work, see below:
    So mainly right now the features i'm thinking of for birdyBot have to do with DMing users on a linux system.
    To do that obviously, we'll need the streaming thing to work, which, it does but i'm not sure what dict or list tweepy is putting direct message data out to.
    I don't want to do my own parsing if I don't have to, and since I know tweepy is, I'd just like to use that.

---
+++ userstream.py
@@ -0,0 +1,47 @@
+# Small utility to stream for the user that's authenticated
+# Triggers and other stuff coming later
+import tweepy
+from secrets import * # import app keys
+from keys import * #import our user keys
+# do auth stuff
+auth = tweepy.OAuthHandler(c_key, c_secret) 
+auth.set_access_token(token, token_secret)
+api = tweepy.API(auth) # Get our API object
+
+class StdOutListener(tweepy.StreamListener):
+    def on_connect( self ):
+        print("Connection to twitter established!!")
+
+    def on_disconnect( self, notice ):
+        print("Connection to twitter lost!! : ", notice)
+
+    def on_status( self, status ):
+        print(status)
+        return True
+
+    def on_direct_message( self, status ):
+        print("Direct message received.")
+        try:
+            # This needs to be able to pull the name, screenname, and message into variables, not sure how
+            #Now it's just printing raw JSON, but I'm sure tweepy is doing some parsing...
+            print(status.text)
+            return True
+        except BaseException as e:
+            print("Failed on_direct_message()", str(e))
+
+    def on_error( self, status ):
+        print(status)
+
+
+def main():
+
+    try:
+        print "Starting userstream for "+api.me().name
+        stream = tweepy.Stream(auth, StdOutListener())
+        stream.userstream()
+
+    except BaseException as e:
+        print("Error in main()", e)
+
+if __name__ == '__main__':
+    main()
