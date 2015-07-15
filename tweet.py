7b062f3f951457e82505358d9e68067f413c2497
oliver2213 <oliver2213@me.com>
2015-07-15 01:17:49 -0400

    updated tweet.py... Getting errors.

--- tweet.py
+++ tweet.py
@@ -1,8 +1,12 @@
 # small utility to post tweets
 import sys
+import tweepy
 from secrets import * # import app keys
 from keys import * #import our user keys
+# do auth stuff
 auth = tweepy.OAuthHandler(c_key, c_secret) 
-auth.set_access_token(key, secret)
+auth.set_access_token(token, token_secret)
 api = tweepy.API(auth)
-api.update_status()
+# form our tweet
+tweet = sys.argv[1]
+api.update_status("testing...")
