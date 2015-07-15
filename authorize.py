6a9fbb48626daf2d46c47d1b9434129952cee6f1
riovan <dhruvdropbox@gmail.com>
2015-07-15 10:56:31 +0530

    fixed authorize.py to use keys.py in stead of secrets.py

--- authorize.py
+++ authorize.py
@@ -12,7 +12,7 @@ print "Once you have verifyed Birdybot, please return here and enter the pin twi
 print "Pin:"
 pin = raw_input().strip()
 tokens = auth.get_access_token(verifier=pin)
-tokenfile= open("secrets.py","w")
+tokenfile= open("keys.py","w")
 tokenfile.write(tokens[0]+'\n')
 tokenfile.write(tokens[1]+'\n')
 print "The tokens should now be saved in secrets.py"
