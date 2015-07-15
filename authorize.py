b6749dd82c2c74952c23038b2aa069b0fbbe4b5a
oliver2213 <oliver2213@me.com>
2015-07-15 00:51:39 -0400

    Updated authorize more

--- authorize.py
+++ authorize.py
@@ -8,7 +8,7 @@ authURL = auth.get_authorization_url()
 print "URL retrieved!"
 print "Please use the following URL to get Birdybot authorized:"
 print authURL
-print "Once you have verifyed Birdybot, please return here and enter teh pin twitter gives you."
+print "Once you have verifyed Birdybot, please return here and enter the pin twitter gives you."
 print "Pin:"
 pin = raw_input().strip()
 tokens = auth.get_access_token(verifier=pin)
