73384498c0ca99f3c6a973a9e94518dd799f0be8
oliver2213 <oliver2213@me.com>
2015-07-15 00:50:53 -0400

    updated authorize.py

--- authorize.py
+++ authorize.py
@@ -8,3 +8,11 @@ authURL = auth.get_authorization_url()
 print "URL retrieved!"
 print "Please use the following URL to get Birdybot authorized:"
 print authURL
+print "Once you have verifyed Birdybot, please return here and enter teh pin twitter gives you."
+print "Pin:"
+pin = raw_input().strip()
+tokens = auth.get_access_token(verifier=pin)
+tokenfile= open("secrets.py","w")
+tokenfile.write(tokens[0]+'\n')
+tokenfile.write(tokens[1]+'\n')
+print "The tokens should now be saved in secrets.py"
