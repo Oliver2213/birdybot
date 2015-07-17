f5aaeb6f5fba3149ac0d95c472d108dfb7dd9537
riovan <dhruvdropbox@gmail.com>
2015-07-17 08:03:32 +0530

    added get_api, which returns an api instance.

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""
+Birdybot
+----------------------------------
+A set of scripts to make a twitter bot easier. One example is included in birdybot.py.
+"""
+import tweepy
+import os
+from secrets import c_key, c_secret
+from keys import token, token_secret
+
+def get_api():
+    """authenticates the user. Returns a twitter.API instance."""
+    auth = tweepy.OAuthHandler(c_key, c_secret)
+    auth.set_access_token(token, token_secret)
+    api = tweepy.API(auth)
+    return api
